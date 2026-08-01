0482097978ef6330997b8157870b1f7fdc524383
YastMe <ale4xd@hotmail.com>
2026-08-01 12:25:23 +0200

    Added half pool detection for Will saving throw against fatigue

--- scripts/hooks/actor-sheet.mjs
+++ scripts/hooks/actor-sheet.mjs
@@ -1,5 +1,9 @@
 export function onPf1ActorSheetRender(app, html, data) {
+    if (data.actor.type === "npc" && data.actor.getFlag("core", "sheetClass") !== "pf1.ActorSheetPFNPC") return;
+    if (data.actor.type !== "character" && data.actor.type !== "npc") return;
     const noManeuversDiv = html.find("div.form-group.stacked")[1];
+    if (!noManeuversDiv) return;
+
     const eldritchDissonanceLabel = document.createElement("label");
     eldritchDissonanceLabel.classList.add("checkbox");
     const eldritchDissonanceInput = document.createElement("input");
@@ -13,4 +17,18 @@ export function onPf1ActorSheetRender(app, html, data) {
     else
         data.actor.setFlag("eldritch-dissonance", "ignoreDissonance", false);
     noManeuversDiv.append(eldritchDissonanceLabel);
+
+	const ignoreFatigueLabel = document.createElement("label");
+	ignoreFatigueLabel.classList.add("checkbox");
+	const ignoreFatigueInput = document.createElement("input");
+	ignoreFatigueInput.type = "checkbox";
+	ignoreFatigueInput.name = "flags.eldritch-dissonance.ignoreFatigue";
+	ignoreFatigueInput.id = ignoreFatigueInput.name;
+	ignoreFatigueLabel.appendChild(ignoreFatigueInput);
+	ignoreFatigueLabel.appendChild(document.createTextNode("Ignore Fatigue from low Spell Points"));
+	if (data.actor.getFlag("eldritch-dissonance", "ignoreFatigue"))
+		ignoreFatigueInput.checked = true;
+	else
+		data.actor.setFlag("eldritch-dissonance", "ignoreFatigue", false);
+	noManeuversDiv.append(ignoreFatigueLabel);
 }
